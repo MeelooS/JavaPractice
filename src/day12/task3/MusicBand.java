588bf444b3becc04fd952189286fa420a7126892
Pchel <gips.1997@mail.ru>
2025-01-22 17:50:45 +0300

    Task 12 comleted (Practice12, Practice12 in task3, Practice12 in task4, Practice12 in task5)

---
+++ src/day12/task3/MusicBand.java
@@ -0,0 +1,26 @@
+package day12.task3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//public class MusicBand {
+//    private String name;
+//    private int year;
+//
+////    public MusicBand(String name, int year){
+////        this.name=name;
+////        this.year=year;
+////    }
+//
+//    public int getYear() {
+//        return year;
+//    }
+//
+//    public String toString(){
+//        return ("группа: "+name+", "+"год: "+year+"\n");
+//    }
+//
+//    public static void groupsAfter2000(){
+//
+//    }
+//}
