d4a0917ce377428f87201fa91d59d441e6f84f11
Pchel <gips.1997@mail.ru>
2025-02-25 14:16:35 +0300

    Some changes

---
+++ src/JavaPrecticeMiddle/ЛямбдаВыражения/Test1.java
@@ -0,0 +1,7 @@
+package JavaPrecticeMiddle.ЛямбдаВыражения;
+
+public class Test1 {
+    public static void main(String[] args){
+        //ичпользование лямбда выражения в специальных методах
+    }
+}
